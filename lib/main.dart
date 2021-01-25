79cdbdc08c9db79f3f923a38ca75b1477aade98b
nyeinchansoee <nyeinchansoe.mm@gmail.com>
2021-01-25 23:51:42 +0630

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Quize',
+      title: 'Quiz',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -38,15 +38,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,68 +46,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Icon> scorekeeper = [];
-  // List<String> questions = [
-  //   'ဆာဂ်ီယိုအဂြယ္႐ို၏ မန္စီးတီးကလပ္စံခ်ိန္တင္ဂိုးေပါင္းမွာ (၁၇၈)ဂိုးျဖစ္သည္။(ZawGyi) /  \n \n ဆာဂျီယိုအဂွယ်ရို၏ မန်စီးတီးကလပ်စံချိန်တင်ဂိုးပေါင်းမှာ (၁၇၈)ဂိုးဖြစ်သည်။ (Unicode)',
-  //   'မန္စီးတီးအသင္း၏ ပထဆုံး Football League/Premier League ဖလားရသည့္ႏွစ္မွာ 1936/37 ျဖစ္သည္။(ZawGyi)/ \n \n  မန်စီးတီးအသင်း၏ ပထဆုံး Football League/ Premier League ဖလားရသည့်နှစ်မှာ 1936/37 ဖြစ်သည်။ (Unicode)',
-  //   'အီဒါဆင္၏ ေမြးေန႔မွာ 17 AUGUST 1991 ျဖစ္သည္။ (ZawGyi)/  \n အီဒါဆင်၏ မွေးနေ့မှာ 17 AUGUST 1991 ဖြစ်သည်။ (Unicode)',
-  //   '1 JULY 2016 တြင္ ဖီးလ္ဖိုဒန္၏ ကစားသမားဘဝကို မန္စီးတီးအသင္းတြင္ စတင္ခဲ့သည္။ (ZawGyi)/ \n \n  1 JULY 2016 တွင် ဖီးလ်ဖိုဒန်၏ ကစားသမားဘဝကို မန်စီးတီးအသင်းတွင် စတင်ခဲ့သည်။ (Unicode)',
-  //   'မန္စီးတီးအသင္းသည္  ၁၉၀၃ခုႏွစ္မွ ၂၀၁၈အထိိ FA Cup (၇) ႀကိမ္ရရွိခဲ့ၿပီးျဖစ္သည္။ (ZawGyi)/ \n \n မန်စီးတီးအသင်းသည်  ၁၉၀၃ခုနှစ်မှ ၂၀၁၈အထိိ FA Cup (၇) ကြိမ်ရရှိခဲ့ပြီးဖြစ်သည်။ (Unicode)',
-  //   'မန္စီးတီးအသင္းသည္  ၃ႏွစ္ဆက္ ခ်န္ပီယံရာသီမ်ားမွာ 2016/17, 2017/18, 2018/19 ျဖစ္သည္။ (ZawGyi)/ \n \n  မန်စီးတီးအသင်းသည်  ၃နှစ်ဆက် ချန်ပီယံရာသီများမှာ 2016/17, 2017/18, 2018/19 ဖြစ်သည်။ (Unicode)',
-  //   'PEP GUARDIOLA သည္ ဒိန္းမတ္ ႏိုင္ငံသားျဖစ္သည္။ (ZawGyi)/ \n \n  PEP GUARDIOLA သည် ဒိန်းမတ် နိုင်ငံသားဖြစ်သည်။ (Unicode)',
-  //   'တစ္ရာသီတြင္း clean sheets အမ်ားဆုံးရာသီမွာ 33 (61 matches, 2018/19) ျဖစ္သည္။ (ZawGyi)/ \n \n  တစ်ရာသီတွင်း clean sheets အများဆုံးရာသီမှာ 33 (61 matches, 2018/19) ဖြစ်သည်။ (Unicode)',
-  //   '1 JULY 2017 တြင္ ဘာနာဒိုေဆးဗားသည္ စီီးတီးႏွင့္ စာခ်ဳပ္စတင္ခဲ့သည္။ (ZawGyi)/ \n \n  1 JULY 2017 တွင် ဘာနာဒိုဆေးဗားသည် စီီးတီးနှင့် စာချုပ်စတင်ခဲ့သည်။ (Unicode)',
-  //   'ကီီီီီီီီီီီီီဗင္ဒီဗ႐ိုင္း၏ေမြးေန႔မွာ 28 JUNE 1991 ျဖစ္သည္။(ZawGyi)/ \n \n  ကီီီီီီီီီီီီီဗင်ဒီဗရိုင်း၏မွေးနေ့မှာ 28 JUNE 1991 ဖြစ်သည်။ (Unicode)',
-  //   ' ေမးခြန္းအားလုံး ေျဖဆိုၿပီးပါၿပီ။(ZawGyi)  / \n \n  မေးခွန်းအားလုံး ဖြေဆိုပြီးပါပြီ။ (Unicode)'
-  // ];
-  // List<bool> answers = [
-  //   true,
-  //   true,
-  //   false,
-  //   true,
-  //   false,
-  //   false,
-  //   false,
-  //   true,
-  //   true,
-  //   true,
-  //   true
-  // ];
-  // List<Questions> questions = [
-  //   Questions(
-  //       q: 'ဆာဂ်ီယိုအဂြယ္႐ို၏ မန္စီးတီးကလပ္စံခ်ိန္တင္ဂိုးေပါင္းမွာ (၁၇၈)ဂိုးျဖစ္သည္။(ZawGyi)   \n \n ဆာဂျီယိုအဂွယ်ရို၏ မန်စီးတီးကလပ်စံချိန်တင်ဂိုးပေါင်းမှာ (၁၇၈)ဂိုးဖြစ်သည်။ (Unicode)',
-  //       a: true),
-  //   Questions(
-  //       q: 'မန္စီးတီးအသင္း၏ ပထဆုံး Football League/Premier League ဖလားရသည့္ႏွစ္မွာ 1936/37 ျဖစ္သည္။(ZawGyi) \n \n  မန်စီးတီးအသင်း၏ ပထဆုံး Football League/ Premier League ဖလားရသည့်နှစ်မှာ 1936/37 ဖြစ်သည်။ (Unicode)',
-  //       a: true),
-  //   Questions(
-  //       q: 'အီဒါဆင္၏ ေမြးေန႔မွာ 17 AUGUST 1991 ျဖစ္သည္။ (ZawGyi)  \n အီဒါဆင်၏ မွေးနေ့မှာ 17 AUGUST 1991 ဖြစ်သည်။ (Unicode)',
-  //       a: false),
-  //   Questions(
-  //       q: '1 JULY 2016 တြင္ ဖီးလ္ဖိုဒန္၏ ကစားသမားဘဝကို မန္စီးတီးအသင္းတြင္ စတင္ခဲ့သည္။ (ZawGyi) \n \n  1 JULY 2016 တွင် ဖီးလ်ဖိုဒန်၏ ကစားသမားဘဝကို မန်စီးတီးအသင်းတွင် စတင်ခဲ့သည်။ (Unicode)',
-  //       a: true),
-  //   Questions(
-  //       q: 'မန္စီးတီးအသင္းသည္  ၁၉၀၃ခုႏွစ္မွ ၂၀၁၈အထိိ FA Cup (၇) ႀကိမ္ရရွိခဲ့ၿပီးျဖစ္သည္။ (ZawGyi) \n \n မန်စီးတီးအသင်းသည်  ၁၉၀၃ခုနှစ်မှ ၂၀၁၈အထိိ FA Cup (၇) ကြိမ်ရရှိခဲ့ပြီးဖြစ်သည်။ (Unicode)',
-  //       a: false),
-  //   Questions(
-  //       q: 'မန္စီးတီးအသင္းသည္  ၃ႏွစ္ဆက္ ခ်န္ပီယံရာသီမ်ားမွာ 2016/17, 2017/18, 2018/19 ျဖစ္သည္။ (ZawGyi) \n \n  မန်စီးတီးအသင်းသည်  ၃နှစ်ဆက် ချန်ပီယံရာသီများမှာ 2016/17, 2017/18, 2018/19 ဖြစ်သည်။ (Unicode)',
-  //       a: false),
-  //   Questions(
-  //       q: 'PEP GUARDIOLA သည္ ဒိန္းမတ္ ႏိုင္ငံသားျဖစ္သည္။ (ZawGyi) \n \n  PEP GUARDIOLA သည် ဒိန်းမတ် နိုင်ငံသားဖြစ်သည်။ (Unicode)',
-  //       a: false),
-  //   Questions(
-  //       q: 'တစ္ရာသီတြင္း clean sheets အမ်ားဆုံးရာသီမွာ 33 (61 matches, 2018/19) ျဖစ္သည္။ (ZawGyi) \n \n  တစ်ရာသီတွင်း clean sheets အများဆုံးရာသီမှာ 33 (61 matches, 2018/19) ဖြစ်သည်။ (Unicode)',
-  //       a: true),
-  //   Questions(
-  //       q: '1 JULY 2017 တြင္ ဘာနာဒိုေဆးဗားသည္ စီီးတီးႏွင့္ စာခ်ဳပ္စတင္ခဲ့သည္။ (ZawGyi) \n \n  1 JULY 2017 တွင် ဘာနာဒိုဆေးဗားသည် စီီးတီးနှင့် စာချုပ်စတင်ခဲ့သည်။ (Unicode)',
-  //       a: true),
-  //   Questions(
-  //       q: 'ကီီီီီီီီီီီီီဗင္ဒီဗ႐ိုင္း၏ေမြးေန႔မွာ 28 JUNE 1991 ျဖစ္သည္။(ZawGyi) \n \n  ကီီီီီီီီီီီီီဗင်ဒီဗရိုင်း၏မွေးနေ့မှာ 28 JUNE 1991 ဖြစ်သည်။ (Unicode)',
-  //       a: true),
-  //   Questions(
-  //       q: ' ေမးခြန္းအားလုံး ေျဖဆိုၿပီးပါၿပီ။(ZawGyi)   \n \n  မေးခွန်းအားလုံး ဖြေဆိုပြီးပါပြီ။ (Unicode)',
-  //       a: true),
-  // ];
-
   // int questionnum = 0;
   void addscore(bool scoretype) {
     if (quizbrain.getAnswer() == scoretype) {
@@ -129,31 +58,50 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
     setState(() {
-      quizbrain.next();
+      // quizbrain.next();
+
+      if (questionnum < quizbrain.questions.length - 1) {
+        questionnum++;
+      } else {
+        questionnum = 0;
+        scorekeeper.clear();
+        Alert(
+          context: context,
+          // style: alertStyle,
+          type: AlertType.info,
+          title: "Quiz Complete",
+          desc: "Ready to Re-start Quiz.",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "COOL",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              color: Color.fromRGBO(0, 179, 134, 1.0),
+              radius: BorderRadius.circular(0.0),
+            ),
+          ],
+        ).show();
+      }
     });
-    if (quizbrain.getAnswer() != scoretype) {
-      setState(() {
-        return Alert(
-                context: context,
-                title: "RFLUTTER",
-                desc: "Flutter is awesome.")
-            .show();
-      });
-    }
-    ;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Man City Quizes'),
+        title: Text(
+          'Man City Quiz',
+          style: TextStyle(backgroundColor: Colors.blue),
+          textAlign: TextAlign.center,
+        ),
       ),
       backgroundColor: Colors.lightBlue,
       body: SafeArea(
         child: Container(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Expanded(
@@ -161,23 +109,24 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Center(
-                      child: Text(
-                        quizbrain.getQuestion(),
-                        style: TextStyle(color: Colors.white, fontSize: 18.0),
-                      ),
+                      child: Text(quizbrain.getQuestion(),
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                          textAlign: TextAlign.center),
                     ),
                   )),
               Expanded(
                   child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: FlatButton(
-                    // textColor: Colors.white,
-                    color: Colors.green,
-                    onPressed: () {
-                      addscore(true);
-                    },
-                    child: Text('Yes',
-                        style: TextStyle(color: Colors.white, fontSize: 20.0))),
+                  // textColor: Colors.white,
+                  color: Colors.green,
+                  onPressed: () {
+                    addscore(true);
+                  },
+                  child: Text('Yes',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      textAlign: TextAlign.center),
+                ),
               )),
               Expanded(
                   child: Padding(
--- lib/quizbrain.dart
+++ lib/quizbrain.dart
@@ -1,5 +1,9 @@
 // import 'questions.dart';
 
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+
 class Questions {
   String questionText;
   bool answerText;
@@ -7,53 +11,53 @@ class Questions {
   Questions({this.questionText, this.answerText});
 }
 
-class Quizbrain {
-  int questionnum = 0;
+int questionnum = 0;
 
+class Quizbrain {
   List<Questions> questions = [
     Questions(
         questionText:
-            'ဆာဂ်ီယိုအဂြယ္႐ို၏ မန္စီးတီးကလပ္စံခ်ိန္တင္ဂိုးေပါင္းမွာ (၁၇၈)ဂိုးျဖစ္သည္။(ZawGyi)   \n \n ဆာဂျီယိုအဂွယ်ရို၏ မန်စီးတီးကလပ်စံချိန်တင်ဂိုးပေါင်းမှာ (၁၇၈)ဂိုးဖြစ်သည်။ (Unicode)',
+            '(1/10) \n\n ဆာဂ်ီယိုအဂြယ္႐ို၏ မန္စီးတီးကလပ္စံခ်ိန္တင္ဂိုးေပါင္းမွာ (၁၇၈)ဂိုးျဖစ္သည္။(ZawGyi)   \n \n ဆာဂျီယိုအဂွယ်ရို၏ မန်စီးတီးကလပ်စံချိန်တင်ဂိုးပေါင်းမှာ (၁၇၈)ဂိုးဖြစ်သည်။ (Unicode)',
         answerText: true),
     Questions(
         questionText:
-            'မန္စီးတီးအသင္း၏ ပထဆုံး Football League/Premier League ဖလားရသည့္ႏွစ္မွာ 1936/37 ျဖစ္သည္။(ZawGyi) \n \n  မန်စီးတီးအသင်း၏ ပထဆုံး Football League/ Premier League ဖလားရသည့်နှစ်မှာ 1936/37 ဖြစ်သည်။ (Unicode)',
+            '(2/10) \n\n မန္စီးတီးအသင္း၏ ပထဆုံး Football League/Premier League ဖလားရသည့္ႏွစ္မွာ 1936/37 ျဖစ္သည္။(ZawGyi) \n \n  မန်စီးတီးအသင်း၏ ပထဆုံး Football League/ Premier League ဖလားရသည့်နှစ်မှာ 1936/37 ဖြစ်သည်။ (Unicode)',
         answerText: true),
     Questions(
         questionText:
-            'အီဒါဆင္၏ ေမြးေန႔မွာ 17 AUGUST 1991 ျဖစ္သည္။ (ZawGyi)  \n အီဒါဆင်၏ မွေးနေ့မှာ 17 AUGUST 1991 ဖြစ်သည်။ (Unicode)',
+            '(3/10) \n\n အီဒါဆင္၏ ေမြးေန႔မွာ 17 AUGUST 1991 ျဖစ္သည္။ (ZawGyi)  \n အီဒါဆင်၏ မွေးနေ့မှာ 17 AUGUST 1991 ဖြစ်သည်။ (Unicode)',
         answerText: false),
     Questions(
         questionText:
-            '1 JULY 2016 တြင္ ဖီးလ္ဖိုဒန္၏ ကစားသမားဘဝကို မန္စီးတီးအသင္းတြင္ စတင္ခဲ့သည္။ (ZawGyi) \n \n  1 JULY 2016 တွင် ဖီးလ်ဖိုဒန်၏ ကစားသမားဘဝကို မန်စီးတီးအသင်းတွင် စတင်ခဲ့သည်။ (Unicode)',
+            '(4/10) \n\n 1 JULY 2016 တြင္ ဖီးလ္ဖိုဒန္၏ ကစားသမားဘဝကို မန္စီးတီးအသင္းတြင္ စတင္ခဲ့သည္။ (ZawGyi) \n \n  1 JULY 2016 တွင် ဖီးလ်ဖိုဒန်၏ ကစားသမားဘဝကို မန်စီးတီးအသင်းတွင် စတင်ခဲ့သည်။ (Unicode)',
         answerText: true),
     Questions(
         questionText:
-            'မန္စီးတီးအသင္းသည္  ၁၉၀၃ခုႏွစ္မွ ၂၀၁၈အထိိ FA Cup (၇) ႀကိမ္ရရွိခဲ့ၿပီးျဖစ္သည္။ (ZawGyi) \n \n မန်စီးတီးအသင်းသည်  ၁၉၀၃ခုနှစ်မှ ၂၀၁၈အထိိ FA Cup (၇) ကြိမ်ရရှိခဲ့ပြီးဖြစ်သည်။ (Unicode)',
+            '(5/10) \n\n မန္စီးတီးအသင္းသည္  ၁၉၀၃ခုႏွစ္မွ ၂၀၁၈အထိိ FA Cup (၇) ႀကိမ္ရရွိခဲ့ၿပီးျဖစ္သည္။ (ZawGyi) \n \n မန်စီးတီးအသင်းသည်  ၁၉၀၃ခုနှစ်မှ ၂၀၁၈အထိိ FA Cup (၇) ကြိမ်ရရှိခဲ့ပြီးဖြစ်သည်။ (Unicode)',
         answerText: false),
     Questions(
         questionText:
-            'မန္စီးတီးအသင္းသည္  ၃ႏွစ္ဆက္ ခ်န္ပီယံရာသီမ်ားမွာ 2016/17, 2017/18, 2018/19 ျဖစ္သည္။ (ZawGyi) \n \n  မန်စီးတီးအသင်းသည်  ၃နှစ်ဆက် ချန်ပီယံရာသီများမှာ 2016/17, 2017/18, 2018/19 ဖြစ်သည်။ (Unicode)',
+            '(6/10) \n\n မန္စီးတီးအသင္းသည္  ၃ႏွစ္ဆက္ ခ်န္ပီယံရာသီမ်ားမွာ 2016/17, 2017/18, 2018/19 ျဖစ္သည္။ (ZawGyi) \n \n  မန်စီးတီးအသင်းသည်  ၃နှစ်ဆက် ချန်ပီယံရာသီများမှာ 2016/17, 2017/18, 2018/19 ဖြစ်သည်။ (Unicode)',
         answerText: false),
     Questions(
         questionText:
-            'PEP GUARDIOLA သည္ ဒိန္းမတ္ ႏိုင္ငံသားျဖစ္သည္။ (ZawGyi) \n \n  PEP GUARDIOLA သည် ဒိန်းမတ် နိုင်ငံသားဖြစ်သည်။ (Unicode)',
+            '(7/10) \n\n PEP GUARDIOLA သည္ ဒိန္းမတ္ ႏိုင္ငံသားျဖစ္သည္။ (ZawGyi) \n \n  PEP GUARDIOLA သည် ဒိန်းမတ် နိုင်ငံသားဖြစ်သည်။ (Unicode)',
         answerText: false),
     Questions(
         questionText:
-            'တစ္ရာသီတြင္း clean sheets အမ်ားဆုံးရာသီမွာ 33 (61 matches, 2018/19) ျဖစ္သည္။ (ZawGyi) \n \n  တစ်ရာသီတွင်း clean sheets အများဆုံးရာသီမှာ 33 (61 matches, 2018/19) ဖြစ်သည်။ (Unicode)',
+            '(8/10) \n\n တစ္ရာသီတြင္း clean sheets အမ်ားဆုံးရာသီမွာ 33 (61 matches, 2018/19) ျဖစ္သည္။ (ZawGyi) \n \n  တစ်ရာသီတွင်း clean sheets အများဆုံးရာသီမှာ 33 (61 matches, 2018/19) ဖြစ်သည်။ (Unicode)',
         answerText: true),
     Questions(
         questionText:
-            '1 JULY 2017 တြင္ ဘာနာဒိုေဆးဗားသည္ စီီးတီးႏွင့္ စာခ်ဳပ္စတင္ခဲ့သည္။ (ZawGyi) \n \n  1 JULY 2017 တွင် ဘာနာဒိုဆေးဗားသည် စီီးတီးနှင့် စာချုပ်စတင်ခဲ့သည်။ (Unicode)',
+            '(9/10) \n\n 1 JULY 2017 တြင္ ဘာနာဒိုေဆးဗားသည္ စီီးတီးႏွင့္ စာခ်ဳပ္စတင္ခဲ့သည္။ (ZawGyi) \n \n  1 JULY 2017 တွင် ဘာနာဒိုဆေးဗားသည် စီီးတီးနှင့် စာချုပ်စတင်ခဲ့သည်။ (Unicode)',
         answerText: true),
     Questions(
         questionText:
-            'ကီီီီီီီီီီီီီဗင္ဒီဗ႐ိုင္း၏ေမြးေန႔မွာ 28 JUNE 1991 ျဖစ္သည္။(ZawGyi) \n \n  ကီီီီီီီီီီီီီဗင်ဒီဗရိုင်း၏မွေးနေ့မှာ 28 JUNE 1991 ဖြစ်သည်။ (Unicode)',
+            '(10/10) \n\n ကီီီီီီီီီီီီီဗင္ဒီဗ႐ိုင္း၏ေမြးေန႔မွာ 28 JUNE 1991 ျဖစ္သည္။(ZawGyi) \n \n  ကီီီီီီီီီီီီီဗင်ဒီဗရိုင်း၏မွေးနေ့မှာ 28 JUNE 1991 ဖြစ်သည်။ (Unicode)',
         answerText: true),
     Questions(
         questionText:
-            ' ေမးခြန္းအားလုံး ေျဖဆိုၿပီးပါၿပီ။(ZawGyi)   \n \n  မေးခွန်းအားလုံး ဖြေဆိုပြီးပါပြီ။ (Unicode)',
+            'Thank You! \n\n ေမးခြန္းအားလုံး ေျဖဆိုၿပီးပါၿပီ။(ZawGyi)   \n \n  မေးခွန်းအားလုံး ဖြေဆိုပြီးပါပြီ။ (Unicode)',
         answerText: true),
   ];
 
@@ -63,11 +67,5 @@ class Quizbrain {
 
   bool getAnswer() {
     return questions[questionnum].answerText;
-  }
-
-  void next() {
-    if (questionnum < questions.length) {
-      questionnum++;
-    }
   }
 }
